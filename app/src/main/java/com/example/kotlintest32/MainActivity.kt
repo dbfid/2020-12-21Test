c391c54a138d76ef72b4d1941b9b7bc13b1bb4fc
dbfid <tkdan3024@naver.com>
2020-12-24 08:31:26 +0900

    2020-12-24 코틀린 공부
    소스_상위 클래스 공부

--- app/src/main/java/com/example/kotlintest32/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest32/MainActivity.kt
@@ -8,4 +8,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    open class Shape{
+        var x: Int = 0
+        set(value){
+            if(value < 0) field = 0
+            else field = value
+        }
+
+        var y: Int = 0
+        set(value){
+            if(value < 0) field = 0
+            else field = value
+        }
+        lateinit var name: String
+
+        fun print() {
+            println("$name : location : $x, $y")
+        }
+    }
 }

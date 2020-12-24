32f35361d8ba4f45c522ff67bca15c0b9162289d
dbfid <tkdan3024@naver.com>
2020-12-24 18:13:43 +0900

    2020-12-24 코틀린 공부
    소스_상위 클래스 공부
    
    open 예약어는 상속을 허용한다는 의미
    
    코틀린에서 클래스의 상속 허용 여부를 결정하는 기본값은 final
    
    개발자가 어떤 클래스를 선언할 때 상속 허용 여부를 명시하지 않으면 기본으로 final로 적용
    
    Rect, Circle 설명
    
    코틀린에서 상속 관계 표현은 콜론( : )을 이용
    
    오버라이드 설명 80 ~ 84, 115
    
    함수 오버라이드 설명
    
    final 클래스, final 함수, final 프로퍼티 95 ~ 97
    
    override 에약어 130
    
    final 예약어
    
    프로퍼티 오버라이드 168
    
    생성자의 흐름을 쉽게 파악하려면 생성자가 하는 역할을 수행 흐름 관점에서 살펴보면 된다.
    
    생성자가 호출되면 다음 순서로 실행된다.
    
    this() 혹은 super()에 의한 다른 생성자 호출
    
    init블록 호출
    
    생성자의()영역 실행
    
    상속과 캐스팅
    
    프로퍼티 getter/setter
    
    is 예약어
    
    public 프로퍼티 -> set(): public, prtected, internal, private
    protected 프로퍼티 -> set(): rpotected, private
    internal 프로퍼티 -> set(); Internal, private
    private 프로퍼티 -> set(): private
    
    생성자와 접근 제한자
    
    코틀린에서 상속은 자바와 다른 부분이 있다.
    
    데이터 클래스
    
    리액티브 프로그래밍
    
    코틀린 copy()
    
    코트린 익명 클래스 이용
    
    Nested 클래스
    
    Object 클래스  -> 중요
    
    object 선언
    
    자바의 싱글톤
    
    코틀린의 싱긍톤
    
    실무 위주로 가는걸로

--- app/src/main/java/com/example/kotlintest32/RxJava.java
+++ app/src/main/java/com/example/kotlintest32/RxJava.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Flow;
 
 public class RxJava extends AppCompatActivity {
 
@@ -16,12 +17,5 @@ public class RxJava extends AppCompatActivity {
     }
     // 리액티브 프로그래밍은 데이터가 통지될 때마다 관련 프로그램이 반응(reaction)해 데이터를 처리하는 프로그래밍 방식
 }
-//소스_자바의 싱글톤
-public class Test{
-    private static Test obj;
-    private Test() { }
-    static Test genInstance(){
-        if(obj == null) obj = new Test();
-        return obj;
-    }
-}
+
+
